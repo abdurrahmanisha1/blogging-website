f5ab2aceb74c4371fab78b8e471cf046d6ff6552
Abdur Rahman IsHa <74505859+abdurrahmanisha1@users.noreply.github.com>
2022-01-24 13:51:18 +0600

    server .js

---
+++ server.js
@@ -0,0 +1,16 @@
+const express = require('express');
+const path = require('path');
+
+
+let initial_path = path.join(__dirname, "puiblic");
+
+const app = express();
+app.use(express.static(initial_path));
+
+app.get('/', (req, res) => {
+    res.sendFile(path.join(initial_path, "home.js"));
+})
+
+app.listen("3000" , () => {
+    console.log('listening......');
+})
